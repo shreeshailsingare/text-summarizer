4562aae1b8c7aef71b84927360b788c58d87d814
Shreeshail Singare <shreeshailsingare5@gmail.com>
2026-05-30 07:20:24 +0530

    Deploy with t5-small

--- app.py
+++ app.py
@@ -9,8 +9,11 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title="Text Summarizer App", description="Text Summarization using T5", version="1.0")
 
-model = T5ForConditionalGeneration.from_pretrained("./saved_summary_model")
-tokenizer = T5Tokenizer.from_pretrained("./saved_summary_model")
+# model = T5ForConditionalGeneration.from_pretrained("./saved_summary_model")
+# tokenizer = T5Tokenizer.from_pretrained("./saved_summary_model")
+
+model = T5ForConditionalGeneration.from_pretrained("t5-small")
+tokenizer = T5Tokenizer.from_pretrained("t5-small")
 
 # device
 if torch.backends.mps.is_available():
